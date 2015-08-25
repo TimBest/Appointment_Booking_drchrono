dbf6a51ec4baec1cb614516b80dfc6f587f88d21
Timothy Best <besttimothy1@gmail.com>
2015-08-25 16:20:46 -0700

    made the appointment select vertical

--- appointment_booking_drchrono/appointment_booking_drchrono/static/scripts/appointments.js
+++ appointment_booking_drchrono/appointment_booking_drchrono/static/scripts/appointments.js
@@ -2,23 +2,23 @@ $("input[name='time_slots']").change(function(){
   $("#id_appointment_date").val($(this).val());
 });
 
-
-/*$(".rotate_table").each(function() {
-    var table = $(this);
-    var newrows = [];
-
-    table.find("tr").each(function(){
-        var i = 0;
-
-        $(this).find("td").each(function(){
-            i++;
-            if(newrows[i] === undefined) { newrows[i] = $(""); }
-            newrows[i].append($(this));
-        });
-    });
-
-    table.find("tr").remove();
-    $.each(newrows, function(){
-        table.append(this);
-    });
-});*/
+$(document).ready(function() {
+  $(".rotate_table").each(function () {
+      var $this = $(this);
+      var newrows = [];
+      $this.find("tr").each(function () {
+          var i = 0;
+          $(this).find("td,th").each(function () {
+              i++;
+              if (newrows[i] === undefined) {
+                  newrows[i] = $("<tr></tr>");
+              }
+              newrows[i].append($(this));
+          });
+      });
+      $this.find("tr").remove();
+      $.each(newrows, function () {
+          $this.append(this);
+      });
+  });
+});
